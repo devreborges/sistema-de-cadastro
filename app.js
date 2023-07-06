8c7f8ab811806c503341e0facf429faa2be1ee24
reBorges <devreborges@gmail.com>
2023-07-05 23:17:39 -0300

    atualizando app.js

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 console.log("Rosando o sistema de gerenciamento de jogos");
+console.log("A aplicação está se conectando ao bando de dados")
+//mais um console log de teste:
+console.log("Enviando para o GitHub")
